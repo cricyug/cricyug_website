96a54cbf91719f6f86887b60daec96e8da5dc86a
cricyug <cricyugapp@gmail.com>
2026-03-14 05:15:06 +0530

    Delete cricyug directory

--- cricyug/database/schema.sql
+++
@@ -1,5 +0,0 @@
-
-CREATE TABLE matches(id SERIAL PRIMARY KEY,team1 TEXT,team2 TEXT,score TEXT,status TEXT,venue TEXT);
-CREATE TABLE players(id SERIAL PRIMARY KEY,name TEXT,country TEXT,runs INT,wickets INT);
-CREATE TABLE teams(id SERIAL PRIMARY KEY,name TEXT,country TEXT);
-CREATE TABLE commentary(id SERIAL PRIMARY KEY,match_id INT,over_no INT,ball_no INT,event TEXT);
